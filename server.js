be8c7bfa086dc52bbff4878f506728f144be6dbb
Rudraraju Tejaswi <153802609+tejaswi23106@users.noreply.github.com>
2026-03-03 01:21:21 +0530

    Refactor server.js for better structure and clarity

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -97,5 +98,5 @@ app.listen(5000, () => {
   console.log("Server running on port 5000");
 });
 app.use("/api/auth", require("./routes"));
-require("dotenv").config();
+
 
